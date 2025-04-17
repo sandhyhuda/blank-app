7600459c556ce92f7d56839cb1b2c85ac7346a69
sandhyhuda <sandhyhuda@gmail.com>
2025-04-17 10:56:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Kalkulator limbah industri")
-st.write( Aplikasi ini digunakan untuk mengidentifikasi limbah industri )."
+st.write("Aplikasi ini digunakan untuk mengidentifikasi limbah industri").
 ()
